9b4c41be5fbd25cfcc94b6302422921786c10cf5
KKandLL-Forever <clay1wang@gmail.com>
2022-03-08 10:38:47 +0800

    feat(准备替换node-sass至dart-sass): 准备替换node-sass至dart-sass
    准备替换node-sass至dart-sass

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template>
 <!--  <button class="w-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">-->
-  <button class="w-button" :class="`icon-${iconPosition}`+`&nbsp;${type}`" @click="$emit('click')">
+  <button class="w-button" :class="[`icon-${iconPosition}`,`${type}`]" @click="$emit('click')">
     <w-icon class="icon" v-if="icon && !loading" :name="icon"></w-icon>
     <w-icon class="loading icon" v-if="loading" name="loading"></w-icon>
     <div class="w-button-content">
@@ -33,13 +33,17 @@
       },
       type: {
         type: String,
-        default: 'default'
+        default: 'default',
+        validate(value){
+          return value === 'default' || value === 'primary' || value === 'danger' || value === 'success' || value === 'info' || value === 'warning'
+        }
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
+//@import "var";
   $font-size: 14px;
   $button-height: 32px;
   $button-bg:white;
@@ -73,6 +77,7 @@
     .loading {
       animation: spin 2s infinite linear;
     }
+    
   }
 
 </style>
